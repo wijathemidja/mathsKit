28c27b83463efb10f13cb85bc2f842445ae73178
Rahul Wijesurendra <wijaking@outlook.com>
2026-01-21 21:04:33 +0000

    added a working nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
   runApp(MaterialApp(home: MainApp()));
 }
 
+// App Main Screen
+
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
@@ -13,9 +15,47 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  List MiniApp = [QuadraticApp(), SettingsApp()];
+  int i = 0;
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(appBar: AppBar(title: Text("Maths Kit")),body: QuadraticSolver()));
+    return Scaffold(
+      body: MiniApp[i],
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.superscript),
+            label: "Quadratic",
+          ),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+        ],
+        selectedIndex: i,
+        onDestinationSelected: (int index) {
+          setState(() {
+            i = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+// App Widgets
+
+class QuadraticApp extends StatefulWidget {
+  const QuadraticApp({super.key});
+
+  @override
+  State<QuadraticApp> createState() => _QuadraticAppState();
+}
+
+class _QuadraticAppState extends State<QuadraticApp> {
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(title: Text("Solve Quadratics")),
+      body: QuadraticSolver(),
+    ));
   }
 }
 
@@ -35,43 +75,58 @@ class _QuadraticSolverState extends State<QuadraticSolver> {
   double quadraticUsrC = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [TextField(
-      controller: _quadraticAText,
-      onSubmitted: (String input){
-        var quadraticUsrAStr = _quadraticAText.text;
-        setState(() {
-          quadraticUsrA = double.parse(quadraticUsrAStr);
-        });
-
-      },
-    ), TextField(
-      controller: _quadraticBText,
-      onSubmitted: (String input){
-        var quadraticUsrBStr = _quadraticBText.text;
-        setState(() {
-          quadraticUsrB = double.parse(quadraticUsrBStr);
-        });
-
-      },
-    ),TextField(
-      controller: _quadraticCText,
-      onSubmitted: (String input){
-        var quadraticUsrCStr = _quadraticCText.text;
-        setState(() {
-          quadraticUsrC = double.parse(quadraticUsrCStr);
-        });
-
-      },
-    ), Text(testSolve(quadraticUsrA, quadraticUsrB, quadraticUsrC))],),
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            controller: _quadraticAText,
+            onSubmitted: (String input) {
+              var quadraticUsrAStr = _quadraticAText.text;
+              setState(() {
+                quadraticUsrA = double.parse(quadraticUsrAStr);
+              });
+            },
+          ),
+          TextField(
+            controller: _quadraticBText,
+            onSubmitted: (String input) {
+              var quadraticUsrBStr = _quadraticBText.text;
+              setState(() {
+                quadraticUsrB = double.parse(quadraticUsrBStr);
+              });
+            },
+          ),
+          TextField(
+            controller: _quadraticCText,
+            onSubmitted: (String input) {
+              var quadraticUsrCStr = _quadraticCText.text;
+              setState(() {
+                quadraticUsrC = double.parse(quadraticUsrCStr);
+              });
+            },
+          ),
+          Text(testSolve(quadraticUsrA, quadraticUsrB, quadraticUsrC)),
+        ],
+      ),
     );
   }
 }
 
+class SettingsApp extends StatelessWidget {
+  const SettingsApp({super.key});
 
-
-
-String testSolve(a,b,c){
-    Quadratic quadq = Quadratic(a:a,b:b,c:c);
-    return(quadq.qslve());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Settings")),
+      body: Text("Settings"),
+    );
+  }
 }
 
+// Functions
+
+String testSolve(a, b, c) {
+  Quadratic quadq = Quadratic(a: a, b: b, c: c);
+  return (quadq.qslve());
+}
